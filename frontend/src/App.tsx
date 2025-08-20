3b92189449d2c16ec1bb6a1554c7628c283ed817
Kyle Minter <kyle.minter.1@gmail.com>
2025-08-19 20:25:48 -0500

    Fixed issues with user authentication frontend and backend integration.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
-import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 import Profile from './Auth0/Profile/Profile';
-import { useEffect } from 'react';
 import AuthenticationButton from './Auth0/AuthenticationButton/AuthenticationButton';
 
 
@@ -15,49 +14,12 @@ function App() {
     console.log(clientId);
     console.log(audience);
 
-    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-
-    useEffect(() => {
-        async function validateUserInDatabase() {
-            console.log('2');
-            try {
-                const token = await getAccessTokenSilently({
-                    authorizationParams: {
-                        audience: audience
-                    }
-                });
-                const response = await fetch(`${audience}/users`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`
-                    },
-                    body: JSON.stringify({
-                        name: `${user!.name}`,
-                        email: `${user!.email}`
-                    })
-                });
-                console.log(response.json());
-            }
-            catch (error) {
-                console.log(error);
-            }
-        }
-        console.log('0');
-
-        if (isAuthenticated) {
-            console.log('1');
-            validateUserInDatabase();
-        }
-    }, [isAuthenticated, getAccessTokenSilently, user, audience]);
-
     return (
         <Auth0Provider
             domain={domain!}
             clientId={clientId!}
             authorizationParams={{
                 redirect_uri: window.location.origin,
-                audience: audience,
             }}
         >
             <div className="App">
