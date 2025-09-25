f25f5b1c6bf68d7924be2c7b3215d058edb6be68
Kyle Minter <kyle.minter.1@gmail.com>
2025-09-25 08:31:13 -0500

    ProfilePage now utilizes UserPage component

--- frontend/src/Pages/ProfilePage/PagePage.tsx
+++ frontend/src/Pages/ProfilePage/PagePage.tsx
@@ -1,11 +1,24 @@
-import Profile from "./Profile";
+import { useEffect, useState } from "react";
+import useAuth from "../../Auth/useAuth";
+import UserPage from "../UserPage/UserPage";
 
 const ProfilePage = () => {
+    const { isEnabled, user, getAccessTokenSilently } = useAuth();
+    const [token, setToken] = useState<string>('');
+    
+    useEffect(() => {
+        (async () => {
+            if (isEnabled)
+                setToken(await getAccessTokenSilently());
+        })();
+    }, [getAccessTokenSilently, isEnabled]);
+
     return (
-        <div>
-            <Profile />
-        </div>
-    )
+        <>
+            <UserPage user={user!}/>
+            <p>{token}</p>
+        </>
+    );
 }
 
 export default ProfilePage;
