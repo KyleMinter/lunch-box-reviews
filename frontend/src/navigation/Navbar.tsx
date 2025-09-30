f0cd722714b68c59ef2c10110df2c4718185c873
Kyle Minter <kyle.minter.1@gmail.com>
2025-09-30 14:56:16 -0500

    Clicking navbar logo/title will navigate back to home page

--- frontend/src/navigation/Navbar.tsx
+++ frontend/src/navigation/Navbar.tsx
@@ -1,12 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './navbar.css'
 import NavbarDropdown from './navbarDropdown/navbarDropdown';
 import SearchBar from "./searchBar/SearchBar";
 
 const Navbar = () => {
+    const navigate = useNavigate();
+
+    const navigateToHome = () => {
+        navigate('/');
+    }
+
     return (
         <header className="navbar">
-            <img src="logo192.png" alt=""/>
-            <h2>Lunch Box Reviews</h2>
+            <div className="navbar-home">
+                <img
+                    onClick={navigateToHome}
+                    src="logo192.png" alt=""
+                />
+                <h2 onClick={navigateToHome}>Lunch Box Reviews</h2>
+            </div>
             <SearchBar />
             <NavbarDropdown />
         </header>
