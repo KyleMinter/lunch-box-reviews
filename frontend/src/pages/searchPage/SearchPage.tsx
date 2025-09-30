e21de2a9b3c92683f62992d18c8300c1d89fe924
Kyle Minter <kyle.minter.1@gmail.com>
2025-09-30 15:04:59 -0500

    Search result page now shows new table components

--- frontend/src/pages/searchPage/SearchPage.tsx
+++ frontend/src/pages/searchPage/SearchPage.tsx
@@ -1,26 +1,34 @@
+import { EntityType, FoodItem, Review, User } from '@lunch-box-reviews/shared-types';
+import UsersTable from '../../components/table/UsersTable';
+import ReviewsTable from '../../components/table/ReviewsTable';
+import FoodTable from '../../components/table/FoodTable';
 import './searchPage.css';
-import SearchResult from './SearchResult';
 
 
 const SearchPage = () => {
-    return (
+        const users: User[] = [
+            {userName: 'name', userEmail: 'email', entityID: 'uid1', entityType: EntityType.User, userPermissions: []},
+            {userName: 'name', userEmail: 'email', entityID: 'uid2', entityType: EntityType.User, userPermissions: []}
+        ];
+
+        const reviews: Review[] = [
+            {entityID: 'rid1', foodID: 'food', menuID: '01/01/2025', userID: 'user', quality: 5.55, quantity: 3, rating: 8, reviewDate: '01/01/2025', entityType: EntityType.Review},
+            {entityID: 'rid2', foodID: 'food', menuID: '01/01/2025', userID: 'user', quality: 5.55, quantity: 3, rating: 8, reviewDate: '01/01/2025', entityType: EntityType.Review}
+        ];
+
+        const foods: FoodItem[] = [
+            {entityID: 'fid1', foodName: 'name', foodOrigin: 'origin', foodAttributes: {nutrition: 'nutrition', description: 'description'}, totalRating: 20, numReviews: 2, entityType: EntityType.FoodItem},
+            {entityID: 'fid1', foodName: 'name', foodOrigin: 'origin', foodAttributes: {}, totalRating: 15, numReviews: 3, entityType: EntityType.FoodItem}
+        ];
+
+        return (
         <div className="page-layout">
             <div className="search-results-title">
                 <h1>Search Results</h1>
             </div>
-            <div className="search-results-headers">
-                <h4>Food</h4>
-                <h4>User</h4>
-                <h4>Rating</h4>
-                <h4>Quality</h4>
-                <h4>Quantity</h4>
-                <h4>Date</h4>
-            </div>
-            <div className="search-results">
-                <SearchResult content='one' />
-                <SearchResult content='two'/>
-                <SearchResult content='three'/>
-            </div>
+            <UsersTable users={users} />
+            <ReviewsTable reviews={reviews} />
+            <FoodTable foodItems={foods}/>
         </div>
     )
 }
