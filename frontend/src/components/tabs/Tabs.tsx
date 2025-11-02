3a8f551134d7f8ce5273ed8156d742cc3c9377a5
Kyle Minter <kyle.minter.1@gmail.com>
2025-11-01 19:58:38 -0500

    Improved usability of Tabs component

--- frontend/src/components/tabs/Tabs.tsx
+++ frontend/src/components/tabs/Tabs.tsx
@@ -1,36 +1,60 @@
+import { ReactElement, useState } from 'react';
 import './tabs.css';
+import React from 'react';
 
 
-interface TabsProps {
-    options: TabOption[]
-    onOptionSelect: (option: TabOption) => void,
+export interface TabProps {
+    id: string;
+    header: string;
     children: React.ReactNode;
 }
 
-export interface TabOption {
-    name: string;
-    key: string;
-    selected: boolean;
+export const Tab: React.FC<TabProps> = ({ children }) => {
+    return (<>{children}</>);
+}
+
+interface TabsProps {
+    defaultSelected: string;
+    onTabSelect: (id: string) => void;
+    children: React.ReactElement<TabProps>[];
 }
 
 const Tabs: React.FC<TabsProps> = ({
-    options,
-    onOptionSelect,
+    defaultSelected,
+    onTabSelect,
     children
 }) => {
+    const tabs = React.Children.toArray(children) as ReactElement<TabProps>[];
+
+    if (tabs.find((tab) => tab.props.id === defaultSelected) === undefined)
+        defaultSelected = tabs[0].props.id;
+
+    const [selectedTabId, setSelectedTabId] = useState<string>(defaultSelected);
+    const selectedTab = tabs.find((tab) => tab.props.id === selectedTabId);
+
+    const onSelect = (id: string) => {
+        setSelectedTabId(id);
+        onTabSelect(id);
+    }
+
     return (
         <>
             <div className="tabs-header">
-                {options.map((option) => { return (
-                    <div
-                        onClick={() => onOptionSelect(option)}
-                        className={option.selected ? 'selected-option' : undefined}
-                        key={option.key}
-                    >{option.name}</div>
-                )})}
+                {tabs.map((child) => {
+                    const tab = child.props;
+                    return (
+                        <div
+                            onClick={() => onSelect(tab.id)}
+                            className={selectedTabId === tab.id ? 'selected-tab' : undefined}
+                            key={tab.id}
+                        >
+                            {tab.header}
+                        </div>
+                    )
+                })}
             </div>
             <div className="tabs-content">
-                {children}
+                {selectedTab}
             </div>
         </>
     )
