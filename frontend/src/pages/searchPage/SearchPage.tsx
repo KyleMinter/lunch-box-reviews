e51834558148b62fcff32da9053015026e48f64c
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-07 15:36:17 -0600

    Rebuilt user table and no search results found display with material UI

--- frontend/src/pages/searchPage/SearchPage.tsx
+++ frontend/src/pages/searchPage/SearchPage.tsx
@@ -3,33 +3,56 @@ import UsersTable from '../../components/table/UsersTable';
 import ReviewsTable from '../../components/table/ReviewsTable';
 import FoodTable from '../../components/table/FoodTable';
 import useSearchResults from '../../hooks/useSearchResults';
-import './searchPage.css';
+import { Box, Typography } from '@mui/material';
 
 
 const SearchPage = () => {
-        const { searchResults } = useSearchResults();
-        
-        const Table = () => {
-            switch (searchResults && searchResults[0].entityType) {
-                case EntityType.Review:
-                    return <ReviewsTable reviews={searchResults as Review[]} />;
-                case EntityType.User:
-                    return  <UsersTable users={searchResults as User[]} />;
-                case EntityType.FoodItem:
-                    return <FoodTable foodItems={searchResults as FoodItem[]}/>;
-            }
+  const { searchResults } = useSearchResults();
 
-            return <div>no results</div>;
-        };
+  const Table = () => {
+    switch (searchResults && searchResults[0].entityType) {
+      case EntityType.Review:
+        return <ReviewsTable reviews={searchResults as Review[]} />;
+      case EntityType.User:
+        return <UsersTable users={searchResults as User[]} />;
+      case EntityType.FoodItem:
+        return <FoodTable foodItems={searchResults as FoodItem[]} />;
+    }
 
-        return (
-            <div className="page-layout">
-                <div className="search-results-title">
-                    <h1>Search Results</h1>
-                </div>
-                <Table />
-            </div>
-    )
+    return (
+      <Box sx={{
+        pt: 2,
+        textAlign: 'center',
+        borderTop: 1,
+        borderColor: 'divider'
+      }}>
+        <Typography variant="h6">
+          No results found
+        </Typography>
+        <Typography variant="subtitle1">
+          Adjust your selected filters for more results
+        </Typography>
+      </Box>
+    );
+  };
+
+  return (
+    <Box sx={{
+      mx: 'auto',
+      maxWidth: 950
+    }}>
+      <Box sx={{
+        pt: 3,
+        pb: 2,
+        textAlign: 'center'
+      }}>
+        <Typography variant="h4">
+          Search Results
+        </Typography>
+      </Box>
+      <Table />
+    </Box>
+  )
 }
 
 export default SearchPage;
