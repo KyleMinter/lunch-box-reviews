924abe192ff0cccf7eedb6e90f024d009954a1e8
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-07 16:19:33 -0600

    Rebuilt food table with material UI

--- frontend/src/components/table/FoodTable.tsx
+++ frontend/src/components/table/FoodTable.tsx
@@ -1,83 +1,117 @@
 import { FoodItem } from "@lunch-box-reviews/shared-types";
-import './table.css';
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography
+} from "@mui/material";
+import { Fragment, useState } from "react";
 
 
 interface FoodTableProps {
-    foodItems: FoodItem[]
+  foodItems: FoodItem[]
 }
 
 const FoodTable: React.FC<FoodTableProps> = ({ foodItems }) => {
-    return (
-        <table className="table">
-            <colgroup>
-                <col style={{ width: "42%" }} />
-                <col style={{ width: "42%"}} />
-                <col style={{ width: "16%"}} />
-            </colgroup>
-            <thead>
-                <tr>
-                    <th className="table-header-no-left-border">
-                        Name
-                    </th>
-                    <th>Origin</th>
-                    <th>Average Rating</th>
-                </tr>
-            </thead>
-            <tbody>
-                {foodItems.map((food, index) => {
-                    return (
-                        <FoodEntry key={index} food={food} />
-                    )
-                })}
-            </tbody>
-        </table>
-    )
-}
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-interface FoodEntryProps {
-    food: FoodItem
-}
+  const handleChangePage = (_event: unknown, newPage: number) => {
+    setPage(newPage);
+  }
 
-const FoodEntry: React.FC<FoodEntryProps> = ({ food }) => {
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
-    const attributes = food.foodAttributes;    
-
-    return (
-        <>
-        <tr>
-            <td>{food.foodName}</td>
-            <td>{food.foodOrigin}</td>
-            <td>{food.totalRating / food.numReviews}</td>
-            
-            
-        </tr>
-        {
-            attributes.description || attributes.nutrition
-            ? (
-                <tr>
-                    <td colSpan={3}>
-                        <p className="table-row-subtext">
-                            {
-                                attributes.description ?
-                                attributes.description
-                                : ''
-                            }
-                            <br />
-                            {
-                                attributes.nutrition ?
-                                <span>
-                                    {attributes.nutrition}
-                                </span>
-                                : ''
-                            }
-                        </p>
-                    </td>
-                </tr>
-            ) :
-            null
-        }
-        </>
-    )
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="user search results table">
+          <TableHead>
+            <TableRow>
+              <TableCell
+                key="foodName"
+                align="left"
+                style={{ minWidth: 15 }}
+              >
+                Name
+              </TableCell>
+              <TableCell
+                key="foodOrigin"
+                align="left"
+                style={{ minWidth: 15 }}
+              >
+                Location
+              </TableCell>
+              <TableCell
+                key="averageRating"
+                align="left"
+                style={{ minWidth: 15 }}
+              >
+                Average Rating
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {foodItems
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((food, index) => {
+                return (
+                  <Fragment key={index}>
+                    <TableRow>
+                      <TableCell key={`${index}-name`} align="left">
+                        {food.foodName}
+                      </TableCell>
+                      <TableCell key={`${index}-origin`} align="left">
+                        {food.foodOrigin}
+                      </TableCell>
+                      <TableCell key={`${index}-averageRating`} align="left">
+                        {food.totalRating / food.numReviews}
+                      </TableCell>
+                    </TableRow>
+                    {(food.foodAttributes.description || food.foodAttributes.nutrition) && (
+                      <TableRow>
+                        <TableCell key={`${index}-attributes`} align="left" colSpan={3}>
+                          <Box sx={{ pl: 2 }}>
+                            {food.foodAttributes.description && (
+                              <Typography key={`${index}-description`} variant="body2" color="textSecondary" sx={{ fontStyle: 'italic' }}>
+                                {food.foodAttributes.description}
+                              </Typography>
+                            )}
+                            {food.foodAttributes.nutrition && (
+                              <Typography key={`${index}-nutrition`} variant="body2" color="textSecondary" sx={{ fontStyle: 'italic' }}>
+                                {food.foodAttributes.nutrition}
+                              </Typography>
+                            )}
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    )}
+                  </Fragment>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 50]}
+        component="div"
+        count={2}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  )
 }
 
 export default FoodTable;
