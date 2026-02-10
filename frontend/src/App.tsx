bab94982e73934ff3da230ca80a297180a31ff1b
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-10 10:54:52 -0600

    Add query client to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,66 +14,78 @@ import SearchPage from './pages/searchPage/SearchPage';
 import './pages/pages.css';
 import SearchProvider from './utils/search/SearchProvider';
 import useSearchResults from './hooks/useSearchResults';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 const App = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const { isOpen: isVerifyEmailModalOpen, setIsOpen: setIsVerifyEmailModalOpen } = usePopupElement(searchParams.has('verifyEmail'));
-    const { isOpen: isAuthErrorModalOpen, setIsOpen: setIsAuthErrorModalOpen } = usePopupElement(searchParams.has('authError'));
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { isOpen: isVerifyEmailModalOpen, setIsOpen: setIsVerifyEmailModalOpen } = usePopupElement(searchParams.has('verifyEmail'));
+  const { isOpen: isAuthErrorModalOpen, setIsOpen: setIsAuthErrorModalOpen } = usePopupElement(searchParams.has('authError'));
 
-    const onVerifyEmailModalClose = () => {
-        setIsVerifyEmailModalOpen(false);
-        searchParams.delete('verifyEmail');
-        setSearchParams(searchParams)
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        staleTime: 5 * 60 * 1000 // Stale time 5 minutes to reduce refetching.
+      }
     }
+  });
 
-    const onAuthErrorModalClose = () => {
-        setIsAuthErrorModalOpen(false);
-        searchParams.delete('authError');
-        setSearchParams(searchParams);
-    }
+  const onVerifyEmailModalClose = () => {
+    setIsVerifyEmailModalOpen(false);
+    searchParams.delete('verifyEmail');
+    setSearchParams(searchParams)
+  }
 
-    return (
-        <>
-            <SearchProvider>
-                <Navbar />
-                <PageRoutes />
-            </SearchProvider>
-            <VerifyEmailModal
-                closeOnLossOfFocus={true}
-                isOpen={isVerifyEmailModalOpen}
-                onClose={onVerifyEmailModalClose}
-            />
-            <AuthErrorModal
-                closeOnLossOfFocus={true}
-                isOpen={isAuthErrorModalOpen}
-                onClose={onAuthErrorModalClose}
-            />
-        </>
-    );
+  const onAuthErrorModalClose = () => {
+    setIsAuthErrorModalOpen(false);
+    searchParams.delete('authError');
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <SearchProvider>
+          <Navbar />
+          <PageRoutes />
+        </SearchProvider>
+      </QueryClientProvider>
+      <VerifyEmailModal
+        closeOnLossOfFocus={true}
+        isOpen={isVerifyEmailModalOpen}
+        onClose={onVerifyEmailModalClose}
+      />
+      <AuthErrorModal
+        closeOnLossOfFocus={true}
+        isOpen={isAuthErrorModalOpen}
+        onClose={onAuthErrorModalClose}
+      />
+    </>
+  );
 }
 
 const PageRoutes = () => {
-    const { isLoading: isLoadingAuth } = useAuth()
-    const { isLoading: isLoadingSearch } = useSearchResults();
+  const { isLoading: isLoadingAuth } = useAuth()
+  const { isLoading: isLoadingSearch } = useSearchResults();
 
-    if (isLoadingAuth || isLoadingSearch) {
-        return (
-            <LoadingPage />
-        )
-    }
-
+  if (isLoadingAuth || isLoadingSearch) {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route
-                path="/profile"
-                element={<AuthGuard Component={ProfilePage} />}
-            />
-            <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-    );
+      <LoadingPage />
+    )
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route
+        path="/profile"
+        element={<AuthGuard Component={ProfilePage} />}
+      />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App;
