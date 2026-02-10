f36238ea201b0cf4c35ccb6321ba7288a3733c82
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-10 10:53:48 -0600

    Revmove references to user permissions and adjust tabbing

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -3,46 +3,46 @@ import React from 'react';
 import { Auth0Provider, AppState } from '@auth0/auth0-react';
 
 interface Auth0ProviderWithNavigateProps {
-    children: React.ReactNode;
-    authParams: {
-        domain?: string;
-        clientId?: string;
-        audience?: string;
-    }
-    
+  children: React.ReactNode;
+  authParams: {
+    domain?: string;
+    clientId?: string;
+    audience?: string;
+  }
+
 }
 
 const Auth0ProviderWithNavigate: React.FC<Auth0ProviderWithNavigateProps> = ({ children, authParams }) => {
-    const {
-        domain,
-        clientId,
-        audience
-    } = authParams;
+  const {
+    domain,
+    clientId,
+    audience
+  } = authParams;
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const onRedirectCallback = (appState?: AppState) => {
-        navigate(appState?.returnTo || window.location.pathname);
-    }
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || window.location.pathname);
+  }
 
-    if (!(domain && clientId))
-        return null;
+  if (!(domain && clientId))
+    return null;
 
-    return (
-        <Auth0Provider
-            domain={domain!}
-            clientId={clientId!}
-            authorizationParams={{
-                redirect_uri: window.location.origin,
-                acr_values: `${window.location.origin}${location.pathname}`,
-                audience: audience
-            }}
-            onRedirectCallback={onRedirectCallback}
-        >
-            {children}
-        </Auth0Provider>
-    );
+  return (
+    <Auth0Provider
+      domain={domain!}
+      clientId={clientId!}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        acr_values: `${window.location.origin}${location.pathname}`,
+        audience: audience
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
 };
 
 export default Auth0ProviderWithNavigate;
--- frontend/src/auth/AuthContext.tsx
+++ frontend/src/auth/AuthContext.tsx
@@ -1,32 +1,32 @@
 import { GetTokenSilentlyOptions } from "@auth0/auth0-react";
-import { User, UserPermission } from "@lunch-box-reviews/shared-types";
+import { User } from "@lunch-box-reviews/shared-types";
 import { createContext } from "react";
 
 
 export interface AuthContextInterface {
-    isEnabled: boolean;
-    isAuthenticated: boolean;
-    isLoading: boolean;
-    getAccessTokenSilently: (options?: GetTokenSilentlyOptions | undefined) => Promise<string>;
-    user?: User;
-    login: () => Promise<void>;
-    logout: () => Promise<void>
-    isAuthorized: (permission?: UserPermission | undefined) => boolean;
+  isEnabled: boolean;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  getAccessTokenSilently: (options?: GetTokenSilentlyOptions | undefined) => Promise<string>;
+  user?: User;
+  login: () => Promise<void>;
+  logout: () => Promise<void>
+  isAuthorized: () => boolean;
 };
 
 const stub = (): never => {
-    throw new Error('You forgot to wrap your component in <AuthProvider>.');
+  throw new Error('You forgot to wrap your component in <AuthProvider>.');
 };
 
 const init: AuthContextInterface = {
-    isEnabled: true,
-    isAuthenticated: false,
-    isLoading: true,
-    getAccessTokenSilently: stub,
-    user: undefined,
-    login: stub,
-    logout: stub,
-    isAuthorized: stub
+  isEnabled: true,
+  isAuthenticated: false,
+  isLoading: true,
+  getAccessTokenSilently: stub,
+  user: undefined,
+  login: stub,
+  logout: stub,
+  isAuthorized: stub
 };
 
 const AuthContext = createContext(init);
