559d86cf2ae6d425a20d13a08a5fac9015fe9fa1
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-10 12:33:41 -0600

    Use a global constants file for .env constants instead of drilling them into places

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,24 +1,13 @@
 import { useLocation, useNavigate } from 'react-router-dom';
 import React from 'react';
 import { Auth0Provider, AppState } from '@auth0/auth0-react';
+import { AUTH0_AUDIENCE, AUTH0_CLIENT_ID, AUTH0_DOMAIN, AUTH_ENABLED } from '../constants';
 
 interface Auth0ProviderWithNavigateProps {
   children: React.ReactNode;
-  authParams: {
-    domain?: string;
-    clientId?: string;
-    audience?: string;
-  }
-
 }
 
-const Auth0ProviderWithNavigate: React.FC<Auth0ProviderWithNavigateProps> = ({ children, authParams }) => {
-  const {
-    domain,
-    clientId,
-    audience
-  } = authParams;
-
+const Auth0ProviderWithNavigate: React.FC<Auth0ProviderWithNavigateProps> = ({ children }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -26,23 +15,28 @@ const Auth0ProviderWithNavigate: React.FC<Auth0ProviderWithNavigateProps> = ({ c
     navigate(appState?.returnTo || window.location.pathname);
   }
 
-  if (!(domain && clientId))
+  if (AUTH_ENABLED && !(AUTH0_DOMAIN && AUTH0_CLIENT_ID)) {
     return null;
+  }
 
-  return (
-    <Auth0Provider
-      domain={domain!}
-      clientId={clientId!}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        acr_values: `${window.location.origin}${location.pathname}`,
-        audience: audience
-      }}
-      onRedirectCallback={onRedirectCallback}
-    >
-      {children}
-    </Auth0Provider>
-  );
+  if (AUTH_ENABLED) {
+    return (
+      <Auth0Provider
+        domain={AUTH0_DOMAIN!}
+        clientId={AUTH0_CLIENT_ID!}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          acr_values: `${window.location.origin}${location.pathname}`,
+          audience: AUTH0_AUDIENCE
+        }}
+        onRedirectCallback={onRedirectCallback}
+      >
+        {children}
+      </Auth0Provider>
+    );
+  } else {
+    return (<>{children}</>);
+  }
 };
 
 export default Auth0ProviderWithNavigate;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,34 +8,19 @@ import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
 import AuthProvider from './auth/AuthProvider';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-
-const authParams = {
-    domain: process.env.REACT_APP_AUTH0_DOMAIN,
-    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
-    audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-};
-const authEnabled = (process.env.REACT_APP_AUTH_ENABLE === 'TRUE');
-
-const Application = (
-    <AuthProvider authEnabled={authEnabled} audience={authParams.audience}>
-        <App />
-    </AuthProvider>
+  document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            {
-                authEnabled ?
-                <Auth0ProviderWithNavigate authParams={authParams}>
-                        {Application}
-                </Auth0ProviderWithNavigate>
-                : Application
-            }
-        </BrowserRouter>
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Auth0ProviderWithNavigate>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </Auth0ProviderWithNavigate>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
