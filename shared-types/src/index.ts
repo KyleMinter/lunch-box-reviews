723fc769966151ae8af658c29f64f0fab895f6f0
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-09 22:27:35 -0600

    Rewrote entity schemas using Zod

--- shared-types/src/index.ts
+++ shared-types/src/index.ts
@@ -1,6 +1,25 @@
-function nameof<T>(name: keyof T) {
-  return name;
-}
+import { z } from "zod";
+
+
+const props = <T extends z.ZodObject<any>>(schema: T) => {
+  const keys = Object.keys(schema.shape) as Array<keyof z.infer<T>>;
+  const map = Object.keys(schema.shape).reduce((acc, key) => {
+    return acc;
+  }, [] as { [K in keyof z.infer<T>]: K});
+
+  return {
+    ...map,
+    keys: keys.join(',')
+  };
+};
+
+/*
+  ======================================================================================================
+
+  Entity types
+
+  ======================================================================================================
+*/
 
 /**
  * An enum representing an entity type stored in the Reviews Table.
@@ -9,45 +28,112 @@ function nameof<T>(name: keyof T) {
  * Review
  * User
  * FoodItem
- * Menu
  * ```
  */
 export enum EntityType {
   Review = 'review',
   User = 'user',
-  FoodItem = 'foodItem',
-  Menu = 'menu'
+  FoodItem = 'foodItem'
 }
+export const entityTypeSchema = z.nativeEnum(EntityType);
 
 /**
  * An interface representing an entity stored in the Reviews Table.
  * Contains the unique ID and the entity type of a given entity instance.
  */
-export interface Entity {
-  entityID: string,
-  entityType: EntityType | string
-}
+export type Entity = z.infer<typeof entitySchema>;
+export const entitySchema = z.object({
+  entityId: z.string(),
+  entityType: entityTypeSchema
+});
+export const entityProps = props(entitySchema);
+
+/*
+  ======================================================================================================
+
+  Food types
+
+  ======================================================================================================
+*/
+
 
 /**
- * A class representing the properties of the Entity interface.
- * Also contains a key property listing all of the interface's properties as string.
+ * An interface representing the attributes of a food item stored in the Reviews Table.
+ * Contains a description and the nutritional information of the food item.
  */
-export class EntityProps {
-  static readonly entityID: string = nameof<Entity>('entityID');
-  static readonly entityType: string = nameof<Entity>('entityType');
+export type FoodAttributes = z.infer<typeof foodAttributesSchema>;
+export const foodAttributesSchema = z.object({
+  description: z.string().optional(),
+  nutrition: z.string().optional()
+})
 
-  static readonly keys: string = `
-        ${this.entityID},
-        ${this.entityType}
-    `;
-}
+/**
+ * An interface representing a food item entity stored in the Reviews Table.
+ * Contains the following fields:
+ * ```js
+ * foodName         // the name of the food item.
+ * foodOrigin       // the name of the origin/location the food item came from.
+ * foodAttributes   // an object containing various attributes of a food item.
+ * totalRating      // the total overall rating of the food item.
+ * numReviews       // the number of reviews for the food item.
+ * ```
+ */
+export type FoodItem = z.infer<typeof foodItemSchema>;
+export const foodItemSchema = entitySchema.extend({
+  foodName: z.string(),
+  foodOrigin: z.string(),
+  foodAttributes: foodAttributesSchema,
+  totalRating: z.number(),
+  numReviews: z.number()
+});
+export const foodItemProps = props(foodItemSchema);
+
+/*
+  ======================================================================================================
+
+  User types
+
+  ======================================================================================================
+*/
+
+/**
+ * An interface representing a user entity stored in the Reviews Table.
+ * Contains the following fields:
+ * ```js
+ * userName         // the user name of the user.
+ * userEmail        // the email of the user.
+ * created          // the date of account creation.
+ * userPermissions  // an array containing the various permissions the user has.
+ * ```
+ */
+export type User = z.infer<typeof userSchema>;
+export const userSchema = entitySchema.extend({
+  userName: z.string(),
+  userEmail: z.string(),
+  created: z.string().datetime()
+});
+export const userProps = props(userSchema);
+
+/*
+  ======================================================================================================
+  
+  Review types
+
+  ======================================================================================================
+*/
+
+export const reviewBaseSchema = entitySchema.extend({
+  quality: z.number(),
+  quantity: z.number(),
+  rating: z.number(),
+  reviewDate: z.string().datetime()
+});
 
 /**
  * An interface representing a review prototype entity stored in the Reviews Table.
  * Contains the following fields:
  * ```js
  * foodID       // the ID of the food item that this review is for.
- * menuID       // the ID of the menu that this review is for.
  * userID       // the ID of the user who created the review.
  * quality      // the quality rating of the review.
  * quantity     // the quantity rating of the review.
@@ -55,40 +141,12 @@ export class EntityProps {
  * reviewDate   // the date this review was last edited. Represented as an ISO-8601 string.
  * ```
  */
-export interface ReviewPrototype extends Entity {
-  foodID: string,
-  menuID: string,
-  userID: string,
-  quality: number,
-  quantity: number,
-  rating: number,
-  reviewDate: string
-}
-
-/**
- * A class representing the properties of the ReviewPrototype interface.
- * Also contains a key property listing all of the interface's properties as string.
- */
-export class ReviewPrototypeProps extends EntityProps {
-  static readonly foodID: string = nameof<ReviewPrototype>('foodID');
-  static readonly menuID: string = nameof<ReviewPrototype>('menuID');
-  static readonly userID: string = nameof<ReviewPrototype>('userID');
-  static readonly quality: string = nameof<ReviewPrototype>('quality');
-  static readonly quanitity: string = nameof<ReviewPrototype>('quantity');
-  static readonly rating: string = nameof<ReviewPrototype>('rating');
-  static readonly reviewDate: string = nameof<ReviewPrototype>('reviewDate');
-
-  static readonly keys: string = `
-        ${EntityProps.keys},
-        ${this.foodID},
-        ${this.menuID},
-        ${this.userID},
-        ${this.quality},
-        ${this.quanitity},
-        ${this.rating},
-        ${this.reviewDate},
-    `;
-}
+export type ReviewPrototype = z.infer<typeof reviewPrototypeSchema>;
+export const reviewPrototypeSchema = reviewBaseSchema.extend({
+  foodId: z.string(),
+  userId: z.string()
+});
+export const reviewPrototypeProps = props(reviewPrototypeSchema);
 
 /**
  * An interface representing a review entity stored in the Reviews Table.
@@ -102,203 +160,35 @@ export class ReviewPrototypeProps extends EntityProps {
  * reviewDate   // the date this review was last edited. Represented as an ISO-8601 string.
  * ```
  */
-export interface Review extends Entity {
-  food: FoodItem,
-  user: User,
-  quality: number,
-  quantity: number,
-  rating: number,
-  reviewDate: string
+export type Review = z.infer<typeof reviewDtoSchema>;
+export const reviewDtoSchema = reviewBaseSchema.extend({
+  food: foodItemSchema,
+  user: userSchema
+});
+
+/*
+  ======================================================================================================
+
+  Pagination Response types
+
+  ======================================================================================================
+*/
+
+/**
+ * Interface representing a generic paginated response containing items of type T.
+ * Contains a nextCursor property that is an encoded key string or undefined.
+ */
+export type PaginatedResponse<T> = {
+  items: T[];
+  nextCursor: string | null
 }
 
 /**
- * A class representing the properties of the Review interface.
- * Also contains a key property listing all of the interface's properties as string.
+ * Interface representing pagination query parameters.
+ * Contains a limit and cursor parameter.
  */
-export class ReviewProps extends EntityProps {
-  static readonly food: string = nameof<Review>('food');
-  static readonly user: string = nameof<Review>('user');
-  static readonly quality: string = nameof<Review>('quality');
-  static readonly quanitity: string = nameof<Review>('quantity');
-  static readonly rating: string = nameof<Review>('rating');
-  static readonly reviewDate: string = nameof<Review>('reviewDate');
-
-  static readonly keys: string = `
-        ${EntityProps.keys},
-        ${this.food},
-        ${this.user},
-        ${this.quality},
-        ${this.quanitity},
-        ${this.rating},
-        ${this.reviewDate},
-    `;
-}
-
-/**
- * An interface representing a user entity stored in the Reviews Table.
- * Contains the following fields:
- * ```js
- * userName         // the user name of the user.
- * userEmail        // the email of the user.
- * accountCreate    // the date of account creation.
- * userPermissions  // an array containing the various permissions the user has.
- * ```
- */
-export interface User extends Entity {
-  userName: string;
-  userEmail: string;
-  accountCreated: string;
-  userPermissions: UserPermission[]
-}
-
-/**
- * A class representing the properties of the User interface.
- * Also contains a key property listing all of the interface's properties as string.
- */
-export class UserProps extends EntityProps {
-  static readonly userName: string = nameof<User>('userName');
-  static readonly userEmail: string = nameof<User>('userEmail');
-  static readonly userPermissions: string = nameof<User>('userPermissions');
-
-  static readonly keys: string = `
-        ${EntityProps.keys},
-        ${this.userName},
-        ${this.userEmail},
-        ${this.userPermissions}
-    `;
-}
-
-/**
- * An enum representing a permission a user can be given.
- * Can either one of the following:
- * ```
- * userReviewPermissions
- * adminReviewPermissions
- * admindFoodItemPermissions
- * adminMenuInstancePermissions
- * adminUserPermissions
- * ```
- */
-export enum UserPermission {
-  userReviewPermissions,
-  adminReviewPermissions,
-  adminFoodItemPermissions,
-  adminMenuInstancePermissions,
-  adminUserPermissions
-}
-
-/**
- * An interface representing a food item entity stored in the Reviews Table.
- * Contains the following fields:
- * ```js
- * foodName         // the name of the food item.
- * foodOrigin       // the name of the origin/location the food item came from.
- * foodAttributes   // an object containing various attributes of a food item.
- * totalRating      // the total overall rating of the food item.
- * numReviews       // the number of reviews for the food item.
- * ```
- */
-export interface FoodItem extends Entity {
-  foodName: string,
-  foodOrigin: string,
-  foodAttributes: FoodAttributes,
-  totalRating: number,
-  numReviews: number
-}
-
-/**
- * An interface representing the attributes of a food item stored in the Reviews Table.
- * Contains a description and the nutritional information of the food item.
- */
-export interface FoodAttributes {
-  description?: string,
-  nutrition?: string
-}
-
-/**
- * A class representing the properties of the FoodItem interface.
- * Also contains a key property listing all of the interface's properties as string.
- */
-export class FoodItemProps extends EntityProps {
-  static readonly foodName: string = nameof<FoodItem>('foodName');
-  static readonly foodOrigin: string = nameof<FoodItem>('foodOrigin');
-  static readonly foodAttributes: string = nameof<FoodItem>('foodAttributes');
-  static readonly totalRating: string = nameof<FoodItem>('totalRating');
-  static readonly numReviews: string = nameof<FoodItem>('numReviews');
-
-  static readonly keys: string = `
-        ${EntityProps.keys},
-        ${this.foodName},
-        ${this.foodOrigin},
-        ${this.foodAttributes},
-        ${this.totalRating},
-        ${this.numReviews},
-    `;
-}
-
-/**
- * An interface representing a menu entity stored in the Reviews Table.
- * Contains the following fields:
- * ```js
- * officeLocation   // the office location of the menu.
- * officeCafe       // an optional cafe location of the menu.
- * menuOption       // the menu option of the menu.
- * menuDate         // the date of the menu.
- * ```
- */
-export interface Menu extends Entity {
-  officeLocation: OfficeLocation,
-  officeCafe?: OfficeCafe,
-  menuOption: MenuOption,
-  menuDate: string
-}
-
-/**
- * An enum representing the option a food item can be.
- * Can either one of the following: `MainOption` or `ChilledOption`.
- */
-export enum MenuOption {
-  MainOption,
-  ChilledOption
-}
-
-/**
- * An enum representing the office location of a food item.
- * Can either one of the following: `OKC_HQ`, `Grapevine`, `NTH`.
- */
-export enum OfficeLocation {
-  OKC_HQ,
-  Grapevine,
-  NTH
-}
-
-/**
- * An enum representing the office cafe of a food item.
- * Can either one of the following: `CafeA`, `CafeB`, `CafeC`, `CafeD`, `CafeE`.
- */
-export enum OfficeCafe {
-  CafeA,
-  CafeB,
-  CafeC,
-  CafeD,
-  CafeE,
-}
-
-/**
- * A class representing the properties of the Menu interface.
- * Also contains a key property listing all of the interface's properties as string.
- */
-export class MenuProps extends EntityProps {
-  static readonly officeLocation: string = nameof<Menu>('officeLocation');
-  static readonly officeCafe: string = nameof<Menu>('officeCafe');
-  static readonly menuOption: string = nameof<Menu>('menuOption');
-  static readonly menuDate: string = nameof<Menu>('menuDate');
-
-  static readonly keys: string = `
-        ${EntityProps.keys},
-        ${this.officeLocation},
-        ${this.officeCafe},
-        ${this.menuOption},
-        ${this.menuDate}
-    `;
-}
+export type PaginationParameters = z.infer<typeof paginationParametersSchema>;
+export const paginationParametersSchema = z.object({
+  limit: z.number(),
+  cursor: z.string().optional()
+});
