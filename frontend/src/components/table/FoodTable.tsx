61ee8372926b9d92e8312722056c5c41830b5222
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-14 02:11:29 -0600

    Fix incorrect styling

--- frontend/src/components/table/FoodTable.tsx
+++ frontend/src/components/table/FoodTable.tsx
@@ -1,4 +1,4 @@
-import { TableCell, TableRow } from "@mui/material";
+import { TableCell, TableRow, Typography } from "@mui/material";
 import { useFoodItems } from "../../hooks/useFetch";
 import InfiniteTable from "./InfiniteTable";
 import { FoodItem } from "@lunch-box-reviews/shared-types";
@@ -14,26 +14,32 @@ const FoodTable = () => {
         { id: "origin", label: "Location" },
         { id: "rating", label: "Average Rating" },
       ]}
-      renderRow={(food: FoodItem) => (
-        <>
-          <TableRow>
-            <TableCell>{food.foodName}</TableCell>
-            <TableCell>{food.foodOrigin}</TableCell>
-            <TableCell>
-              {food.totalRating / food.numReviews}
-            </TableCell>
-          </TableRow>
-
-          {(food.foodAttributes.description || food.foodAttributes.nutrition) && (
+      renderRow={(food: FoodItem) => {
+        const { description, nutrition } = food.foodAttributes
+        return (
+          <>
             <TableRow>
-              <TableCell colSpan={3}>
-                {food.foodAttributes.description}
-                {food.foodAttributes.nutrition}
+              <TableCell>{food.foodName}</TableCell>
+              <TableCell>{food.foodOrigin}</TableCell>
+              <TableCell>
+                {food.totalRating / food.numReviews}
               </TableCell>
             </TableRow>
-          )}
-        </>
-      )}
+
+            {(description || nutrition) && (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <Typography variant="body2" color="textSecondary" sx={{ fontStyle: 'italic' }}>
+                    {description}
+                    {description && nutrition && <br />}
+                    {nutrition}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            )}
+          </>
+        )
+      }}
     />
   )
 }
