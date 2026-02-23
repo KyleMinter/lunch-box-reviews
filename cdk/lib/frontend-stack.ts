8555d5502c5fcae4130e24c916d76d8dbbc0f98f
Kyle Minter <kyle.minter.1@gmail.com>
2026-02-23 12:15:12 -0600

    Fix indentation

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -10,63 +10,63 @@ import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class FrontendStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        const websiteBucket = new s3.Bucket(this, 'lunch-box-reviews-frontend', {
-            autoDeleteObjects: true,
-            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-            removalPolicy: RemovalPolicy.DESTROY
-        });
+    const websiteBucket = new s3.Bucket(this, 'lunch-box-reviews-frontend', {
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
 
-        const originAccessIdentity = new OriginAccessIdentity(this, 'OAI', {
-            comment: `OAI for ${id}`
-        });
+    const originAccessIdentity = new OriginAccessIdentity(this, 'OAI', {
+      comment: `OAI for ${id}`
+    });
 
-        websiteBucket.addToResourcePolicy(new iam.PolicyStatement({
-            actions: ['s3:GetObject', 's3:ListBucket'],
-            resources: [websiteBucket.arnForObjects('*'), websiteBucket.bucketArn],
-            principals: [new iam.CanonicalUserPrincipal(originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
-        }));
+    websiteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject', 's3:ListBucket'],
+      resources: [websiteBucket.arnForObjects('*'), websiteBucket.bucketArn],
+      principals: [new iam.CanonicalUserPrincipal(originAccessIdentity.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
 
-        const distribution = new Distribution(this, 'CloudfrontDistribution', {
-            defaultBehavior: {
-                origin: new S3Origin(websiteBucket, {
-                    originAccessIdentity,
-                    originPath: '',
-                }),
-                viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            },
-            defaultRootObject: 'index.html',
-            errorResponses: [
-                {
-                    httpStatus: 404,
-                    responseHttpStatus: 200,
-                    responsePagePath: '/index.html',
-                }
-            ]
-        });
-
-        const assetsPath = path.join(__dirname, '../assets/');
-        if (fs.existsSync(assetsPath)) {
-            new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-                sources: [s3deploy.Source.asset(assetsPath)],
-                destinationBucket: websiteBucket,
-                distribution,
-                distributionPaths: ['/*']
-            })
+    const distribution = new Distribution(this, 'CloudfrontDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(websiteBucket, {
+          originAccessIdentity,
+          originPath: '',
+        }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html',
         }
+      ]
+    });
 
-        new CfnOutput(this, 'CloudFrontURL', {
-            value: distribution.domainName,
-            description: 'The distribution URL',
-            exportName: 'CloudfrontURL',
-        });
-
-        new CfnOutput(this, 'BucketName', {
-            value: websiteBucket.bucketName,
-            description: 'The name of the S3 bucket',
-            exportName: 'BucketName',
-        });
+    const assetsPath = path.join(__dirname, '../assets/');
+    if (fs.existsSync(assetsPath)) {
+      new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+        sources: [s3deploy.Source.asset(assetsPath)],
+        destinationBucket: websiteBucket,
+        distribution,
+        distributionPaths: ['/*']
+      })
     }
+
+    new CfnOutput(this, 'CloudFrontURL', {
+      value: distribution.domainName,
+      description: 'The distribution URL',
+      exportName: 'CloudfrontURL',
+    });
+
+    new CfnOutput(this, 'BucketName', {
+      value: websiteBucket.bucketName,
+      description: 'The name of the S3 bucket',
+      exportName: 'BucketName',
+    });
+  }
 }
